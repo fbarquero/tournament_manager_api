c8510517b3ef4817281a7db0e216a4f3e08eb50a
fbarquero <fbarquero86@gmail.com>
2014-06-17 18:09:15 -0600

    adding stuff to model layer

--- modules/utils.py
+++ modules/utils.py
@@ -1,49 +1,6 @@
-# coding: utf8
-# try something like
 __author__ = 'Mordigan'
 import random
 import math
-import json
-import sys
-from gluon.debug import qdb_debugger
-
-def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    return service()
-
-@request.restful()
-def belt():
-    response.view = 'generic.json'
-    def GET(*args,**vars):
-        patterns = [
-            "/color[belt]"
-        ]
-        parser = db.parse_as_rest(patterns,args,vars)
-        if parser.status == 200:
-            return dict(content=parser.response)
-        else:
-            raise HTTP(parser.status,parser.error)
-
-    def POST(**vars):
-        try:
-             return dict(message = db.belt.bulk_insert(vars['content']))
-        except:
-            raise HTTP (400, "Unexpected error: %s" % sys.exc_info()[0], message = sys.exc_info()[0])
-            #db.commit()
-        #except:
-            #db.rollback()
-    def PUT(**vars):
-        try:
-            db(db.belt.id == 7).validate_and_update(description = 'whiteUpdated')
-            db.commit()
-        except:
-            db.rollback()
-    return locals()
 
 def single_elimination_bracket_generation(players):
     match_maker(shuffle_players(players))
@@ -109,13 +66,6 @@ def match_maker(shuffled_players):
                     matches['results'].append([0, 0])
                     count += 1
                 count += 1
-        #bracket.append(matches)
-        #print "Byed Players:"
-        #print byed_players
-        #print "shufled players after bye process:"
-        #print shuffled_players
-    #bracket = generate_next_rounds(len(shuffled_players), bracket, byed_players)
-    #bracket.append([None])
     print "Matches dict:"
     print matches
     return matches
@@ -140,10 +90,6 @@ def get_exponent_base_two(number):
         number /= 2
         exponent += 1
     return exponent
-
-#single_elimination_bracket_generation(["alonso", "darian", "michael", "lunita", "5", "6", "7", "8", "alonso2", "darian2", "michael2", "lunita2", "5_2", "6_2", "7_2", "8_2"])
-#single_elimination_bracket_generation(["alonso", "darian", "michael", "josue", "lucia", "servet tezagul"])
-#single_elimination_bracket_generation(["alonso", "michael", "aaron cook"])
-#single_elimination_bracket_generation(["Aroon cook", "Tezaqul", "Tuncat", "Steven L", "nikpah"])
 #single_elimination_bracket_generation(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15","16"])
 #single_elimination_bracket_generation(["1", "2", "3", "4", "5", "6"])
+
